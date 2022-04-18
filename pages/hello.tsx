a3e1c3ab75a1cc8f4ca40f0d44fdf7b8e49d8581
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2022-04-18 13:29:12 +0900

    add fetching data with api

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,19 +1,25 @@
 import type { NextPage, GetStaticProps } from 'next'
 
+type Data = {
+  title: string
+}
 type Props = {
   now: string
+  data: Data
 }
 
-export const getStaticProps: GetStaticProps<Props> = () => {
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data: Data = await fetch("https://hacker-news.firebaseio.com/v0/item/31000386.json")
+    .then((res) => res.json())
   const now = new Date().toISOString()
-  return {  props: {now: now} }
+  return {  props: { now, data } }
 }
 
 
 const Page: NextPage<Props> = (props) => {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>{props.data.title}</h1>
       <p>{props.now}</p>
     </div>
   )
